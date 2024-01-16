6cd5efc655e6ccf99cce90cfd0f6dbcca43b12a4
Resuragam <1791614500@qq.com>
2024-01-16 19:58:47 +0800

    feat: 寻物失物招领记录跳转

--- miniprogram/pages/my/myPage/index.js
+++ miniprogram/pages/my/myPage/index.js
@@ -1,66 +1,18 @@
 // pages/my/myPage/index.js
 Page({
-
-    /**
-     * 页面的初始数据
-     */
-    data: {
-
+    toInfo() {
+      wx.navigateTo({
+        url: '/pages/my/info/index',
+      })
     },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
-
+    toFoundRecord() {
+      wx.navigateTo({
+        url: '/pages/my/foundRecord/index',
+      })
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
+    toLostRecord() {
+      wx.navigateTo({
+        url: '/pages/my/lostRecord/index',
+      })
     }
 })
