7c8fbc9919f6dbbe7485b29a68261a214327efa8
Resuragam <1791614500@qq.com>
2024-03-07 00:32:54 +0800

    fix: 调整发布寻物需求页面样式

--- miniprogram/pages/issuance/publishLost/index.js
+++ miniprogram/pages/issuance/publishLost/index.js
@@ -1,10 +1,9 @@
-// pages/issuance/publishFound/index.js
+// pages/issuance/publishLost/index.js
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    style: "border: 2rpx solid rgba(220,220,220,1);border-radius: 12rpx;",
     mode: "",
     datetimeVisible: false,
     datetime: new Date().getTime(),
