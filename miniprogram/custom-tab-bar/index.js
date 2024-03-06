739d28136575c68d3f08d79deefe8a76b43642a5
Resuragam <1791614500@qq.com>
2024-03-06 17:51:38 +0800

    feat: 新增自定义底部导航栏组件以及样式

---
+++ miniprogram/custom-tab-bar/index.js
@@ -0,0 +1,39 @@
+// custom-tab-bar/index.js
+// custom-tab-bar/index.ts
+Component({
+  //  组件的属性列表
+  properties: {},
+  //  组件的初始数据
+  data: {
+    value: 0,
+    list: [
+      {
+        url: "/pages/index/index"
+      },
+      {
+        url: "/pages/test/index"
+      }
+    ],
+    visible: false
+  },
+  //  组件的方法列表
+  methods: {
+    onChange(e) {
+      this.setData({ value: e.detail.value });
+      wx.switchTab({
+        url: this.data.list[e.detail.value].url
+      });
+    },
+    init() {
+      const page = getCurrentPages().pop();
+      this.setData({
+        value: this.data.list.findIndex(item => item.url === `/${page?.route}`)
+      });
+    },
+    handleClick() {
+      this.setData({
+        visible: true
+      });
+    }
+  }
+});
