3f250dcf6cf042a6f35d53cd735d61e2f0748a3b
Resuragam <1791614500@qq.com>
2024-03-18 18:00:21 +0800

    feat: 新增修改个人信息云函数接口

---
+++ cloudfunctions/updateUserInfo/index.js
@@ -0,0 +1,29 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
+
+const db = cloud.database()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+    try {
+      db.collection('user').where({
+        openId: event.openId
+      }).update({
+        nickname: event.nickname,
+        phoneNumber: event.phoneNumber,
+        avatar: event.avatar
+      })
+    }catch(e) {
+      console.error(e)
+    }
+    const wxContext = cloud.getWXContext()
+
+    return {
+        event,
+        openid: wxContext.OPENID,
+        appid: wxContext.APPID,
+        unionid: wxContext.UNIONID,
+    }
+}
