7d196d5685f5f0071d863a606bd5f83db269f55c
Resuragam <1791614500@qq.com>
2024-03-20 14:41:41 +0800

    feat: 新增个人发布页面接口参数

--- miniprogram/pages/issuance/publishFound/index.js
+++ miniprogram/pages/issuance/publishFound/index.js
@@ -1,83 +1,14 @@
 // pages/issuance/publishFound/index.js
 Page({
   /**
-   * 页面的初始数据
+   * 页面数据
    */
   data: {
-    mode: "",
-    datetimeVisible: false,
-    datetime: new Date().getTime(),
-    datetimeText: ""
-  },
-
-  showPicker(e) {
-    const { mode } = e?.currentTarget?.dataset;
-    this.setData({
-      mode,
-      [`${mode}Visible`]: true
-    });
-  },
-  hidePicker() {
-    const { mode } = this.data;
-    this.setData({
-      [`${mode}Visible`]: false
-    });
-  },
-  onConfirm(e) {
-    const { value } = e?.detail;
-    const { mode } = this.data;
-
-    console.log("confirm", value);
-
-    this.setData({
-      [mode]: value,
-      [`${mode}Text`]: value
-    });
-
-    this.hidePicker();
-  },
-
-  onColumnChange(e) {
-    console.log("pick", e?.detail?.value);
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {},
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {},
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {},
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {},
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {},
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {},
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {},
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {}
+    title: "",
+    time: "",
+    address: "",
+    phoneNumber: "",
+    desc: "",
+    imageList: []
+  }
 });
