1f7a5d257671f476690c60afbd5656aed222fa1f
Resuragam <1791614500@qq.com>
2024-04-16 22:06:53 +0800

    feat: 新增范围查询页面场地信息获取和跳转

--- miniprogram/pages/range/index.js
+++ miniprogram/pages/range/index.js
@@ -1,11 +1,40 @@
 // pages/range/index.js
+import { getSpaceList } from '../../api/sapce'
 Page({
   data: {
-    value: [0, 1],
-    value1: [0, 1],
-    checkedNum: 2
+    checkedNum: 0,
+    spaceList: [],
+    checkedSpaceList: []
   },
-  onChange1(e) {
-    this.setData({ value1: e.detail.value });
+  onSpaceTagStatusChange(e) {
+    let { checkedNum, checkedSpaceList } = this.data
+    const { checked } = e.detail
+    const { space } = e.currentTarget.dataset
+    if(checked) {
+      checkedSpaceList.push(space)
+      checkedNum = checkedNum + 1
+    }else {
+      checkedSpaceList = checkedSpaceList.filter(s => s!== space)
+      checkedNum = checkedNum - 1
+    }
+    this.setData({
+      checkedNum,
+      checkedSpaceList,
+    })
+    console.log(checkedSpaceList, e)
+  },
+  onLoad() {
+    this.initSpaceList()
+  },
+  async initSpaceList() {
+    const res = await getSpaceList()
+    this.setData({
+      spaceList: res.result.data
+    })
+  },
+  toSpace() {
+    wx.navigateTo({
+      url: `/pages/space/index?space=${this.data.checkedSpaceList}`,
+    })
   }
 });
