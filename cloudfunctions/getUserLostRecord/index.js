1576209047927a27a587e6ef185036e352c12697
Resuragam <1791614500@qq.com>
2024-04-22 19:32:01 +0800

    feat: 新增寻物失物云函数接口

---
+++ cloudfunctions/getUserLostRecord/index.js
@@ -0,0 +1,46 @@
+// 云函数入口文件
+const cloud = require("wx-server-sdk");
+
+cloud.init({ env: "lost-found-9gf6k1tce66b0eb1" }); // 使用当前云环境
+
+const db = cloud.database();
+// 云函数入口函数
+exports.main = async (event, context) => {
+  try {
+    // 连接失物记录表和用户表
+    const lostRecords = await db
+      .collection("lost_record")
+      .aggregate() // 使用聚合操作来实现联表查询
+      .lookup({
+        from: "user", // 指定要连接的集合（用户表）
+        localField: "openId", // 失物记录表中用于连接的字段
+        foreignField: "openId", // 用户表中用于连接的字段
+        as: "userInfo" // 连接后的用户信息存储在 userInfo 字段
+      }).match({
+        openId: event.openId
+      })
+      .end();
+
+    // 处理返回的数据，只保留需要的用户信息字段
+    const processedRecords = lostRecords.list.map(record => {
+      if (record.userInfo.length > 0) {
+        const user = record.userInfo[0];
+        record.userAvatar = user.avatar; // 用户头像
+        record.userNickname = user.nickname; // 用户昵称
+      }
+      delete record.userInfo; // 删除原始的 userInfo 字段
+      return record;
+    });
+
+    return {
+      code: 0,
+      data: processedRecords,
+      message: "查询成功"
+    };
+  } catch (e) {
+    return {
+      code: -1,
+      message: `查询失败：${e}`
+    };
+  }
+};
