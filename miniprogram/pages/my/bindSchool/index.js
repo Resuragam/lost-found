daac476e03a1fe04f038088a4550e628eb86e3c9
Resuragam <1791614500@qq.com>
2024-05-21 16:11:48 +0800

    fix: 修复部分bug

--- miniprogram/pages/my/bindSchool/index.js
+++ miniprogram/pages/my/bindSchool/index.js
@@ -3,45 +3,23 @@ Page({
   /**
    * 页面的初始数据
    */
-  data: {},
+  data: {
+    college: "",
+    studentNumber: ""
+  },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {},
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {},
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {},
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {},
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {},
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {},
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {},
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {}
+  bindSchool() {
+    if (!this.data.college) {
+      return wx.showToast({
+        title: "请输入学院信息",
+        icon: "none"
+      });
+    }
+    if (!this.data.studentNumber) {
+      return wx.showToast({
+        title: "请输入学号信息",
+        icon: "none"
+      });
+    }
+  }
 });
